8f137abcbc6972cec0e7f3f2cb7cb0da09c135a8
yaser ehsani <ehsaniyaser@yahoo.com>
2019-06-10 23:54:21 +0430

    Updates

---
+++ precache-manifest.99d41ea7089c86061014cea11384dc0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ce956a77045742395b691c8d55cf1d",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "2d21d52c47d70b2465f2",
+    "url": "/maktabkhoone-resume/static/css/main.54bbdec0.chunk.css"
+  },
+  {
+    "revision": "8db51d399a6b4fce8a84",
+    "url": "/maktabkhoone-resume/static/js/2.82a63a8b.chunk.js"
+  },
+  {
+    "revision": "2d21d52c47d70b2465f2",
+    "url": "/maktabkhoone-resume/static/js/main.afbeda50.chunk.js"
+  },
+  {
+    "revision": "b1689e7ccd28ad118f22",
+    "url": "/maktabkhoone-resume/static/js/runtime~main.65c4976a.js"
+  }
+]);
